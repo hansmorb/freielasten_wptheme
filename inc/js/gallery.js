09efdbf309de369f2f1082704d7cc7931b632d03
HansMorb <6433480+hansmorb@users.noreply.github.com>
2021-12-22 23:07:22 +0100

    changed files

--- inc/js/gallery.js
+++ inc/js/gallery.js
@@ -1,35 +1,28 @@
-var slideIndex = new Array(galleryIterator);
-var slideId = new Array(galleryIterator);
-slideIndex.foreach(function(item,index,array){
-  slideIndex[index] = 1;
-  slideId[index] = "gallery" + index;
-});
-slideIndex.foreach(function(item,index,array){
-  showSlides(1, index);
-});
+var slideIndex = 1;
+showSlides(slideIndex);
 
 // Next/previous controls
-function plusSlides(n, no) {
-  showSlides(slideIndex[no] += n, no);
+function plusSlides(n) {
+	showSlides(slideIndex += n);
 }
 
 // Thumbnail image controls
-function currentSlide(n, no) {
-	showSlides(slideIndex = n, no);
+function currentSlide(n) {
+	showSlides(slideIndex = n);
 }
 
-function showSlides(n, no) {
-  var i;
-  var slides = document.getElementsByClassName(slideId[no]);
-	var dots = document.getElementsByClassName("dot" + no);
-  if (n > slides.length) {slideIndex[no] = 1}
-  if (n < 1) {slideIndex[no] = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-	for (i = 0; i < dots.length; i++){
+function showSlides(n) {
+	var i;
+	var slides = document.getElementsByClassName("itemgallery");
+	var dots = document.getElementsByClassName("dot");
+	if (n > slides.length) {slideIndex = 1}
+	if (n < 1) {slideIndex = slides.length}
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	for (i = 0; i < dots.length; i++) {
 		dots[i].className = dots[i].className.replace(" active", "");
 	}
-  slides[slideIndex[no]-1].style.display = "block";
+	slides[slideIndex-1].style.display = "block";
 	dots[slideIndex-1].className += " active";
 }
--- inc/js/itemGallery.js
+++ inc/js/itemGallery.js
@@ -1,28 +1,35 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+var slideIndex = new Array(galleryIterator);
+var slideId = new Array(galleryIterator);
+slideIndex.foreach(function(item,index,array){
+  slideIndex[index] = 1;
+  slideId[index] = "gallery" + index;
+});
+slideIndex.foreach(function(item,index,array){
+  showSlides(1, index);
+});
 
 // Next/previous controls
-function plusSlides(n) {
-	showSlides(slideIndex += n);
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
 }
 
 // Thumbnail image controls
-function currentSlide(n) {
-	showSlides(slideIndex = n);
+function currentSlide(n, no) {
+	showSlides(slideIndex = n, no);
 }
 
-function showSlides(n) {
-	var i;
-	var slides = document.getElementsByClassName("itemgallery");
-	var dots = document.getElementsByClassName("dot");
-	if (n > slides.length) {slideIndex = 1}
-	if (n < 1) {slideIndex = slides.length}
-	for (i = 0; i < slides.length; i++) {
-		slides[i].style.display = "none";
-	}
-	for (i = 0; i < dots.length; i++) {
+function showSlides(n, no) {
+  var i;
+  var slides = document.getElementsByClassName(slideId[no]);
+	var dots = document.getElementsByClassName("dot" + no);
+  if (n > slides.length) {slideIndex[no] = 1}
+  if (n < 1) {slideIndex[no] = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+	for (i = 0; i < dots.length; i++){
 		dots[i].className = dots[i].className.replace(" active", "");
 	}
-	slides[slideIndex-1].style.display = "block";
+  slides[slideIndex[no]-1].style.display = "block";
 	dots[slideIndex-1].className += " active";
 }
