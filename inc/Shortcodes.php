58e9a5b34875191c780160b0f272152e1a84fb96
HansMorb <6433480+hansmorb@users.noreply.github.com>
2021-12-26 16:15:40 +0100

    show only items with specific coupling now possible in postgrid

--- inc/Shortcodes.php
+++ inc/Shortcodes.php
@@ -13,12 +13,20 @@ function shortcode_postGridfromCategory($atts){
 		'itemcat' => '',
 	  'locationcat' => '',
 		'hidedefault' => 'false',
-    'sortbyavailability' => 'true'
+    'sortbyavailability' => 'true',
+		'kupplung' => ''
 	),$atts);
 	$atts['hidedefault'] = filter_var( $atts['hidedefault'], FILTER_VALIDATE_BOOLEAN );
   $atts['sortbyavailability'] = filter_var( $atts['sortbyavailability'], FILTER_VALIDATE_BOOLEAN);
 
-  $itemList = get_cb_items_by_category($atts['itemcat']);
+
+	/* Mies dreckiger Workaround bis es die Funktion filterPostsByKupplung gibt*/
+	if ($atts['kupplung'] != '') {
+		$itemList = get_post_by_category_and_kupplung($atts['itemcat'],array($atts['kupplung']));
+	}
+	else {
+		$itemList = get_cb_items_by_category($atts['itemcat']);
+	}
 
   if ($atts['locationcat'] != '') {
     $itemList = filterPostsByLocation($itemList,$atts['locationcat']);
@@ -27,6 +35,7 @@ function shortcode_postGridfromCategory($atts){
   if ($atts['sortbyavailability']){
     $itemList = sortItemsByAvailability($itemList);
   }
+
 
 	if ($itemList){
 		return create_postgrid_from_posts($itemList,$atts['hidedefault']); //Hide Default not working, that's why its to always true
